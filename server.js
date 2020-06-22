abba3f11b5cccb7c5fc66a59a70d87ca6aeae305
manish ranjan <manishranjan.thehunk@gmail.com>
2020-06-22 22:28:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,17 +19,18 @@ const forceSSL = function() {
   // Instruct the app
   // to use the forceSSL
   // middleware
-  app.use(forceSSL());
+app.use(forceSSL());
 
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
-// Start the app by listening on the default
-// Heroku port
-app.listen(process.env.PORT || 8080);
+app.use(express.static(__dirname + '/dist/june21'));
 
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-    res.sendFile(path.join('./june21/dist/index.html'));
-  });
+    res.sendFile(path.join(__dirname + '/dist/june21/index.html'));
+});
+
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
