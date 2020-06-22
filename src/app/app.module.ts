40e4dab5651bff2c9e8fa7b39dc6825e2b0e1432
manish ranjan <manishranjan.thehunk@gmail.com>
2020-06-23 02:15:07 +0530

    UI Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
@@ -19,6 +20,7 @@ import { User } from './models/auth/user';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     HttpClientModule
   ],
--- src/app/app_auth/login/login.component.ts
+++ src/app/app_auth/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth/auth.service'
 import { User } from '../../models/auth/user'
+import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -11,10 +12,24 @@ export class LoginComponent implements OnInit {
   loginUserData: User;
   message: string;
 
-  constructor(private _authService: AuthService) { }
+  // public loginForm: FormGroup; = new FormGroup({
+  //   email: new FormControl(null, Validators.required)
+  // })
+  public loginForm: FormGroup;
+
+  constructor(private _fb: FormBuilder, private _authService: AuthService) {
+    this.createForm();
+   }
 
   ngOnInit() {
     this.loginUserData = new User();
+  }
+
+  createForm(){
+    this.loginForm = this._fb.group({
+      email:[null, Validators.required],
+      password: [null, [Validators.required, Validators.minLength(8)]]
+    });
   }
 
   loginUser(){
